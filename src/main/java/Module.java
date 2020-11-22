eda07203c44732d6f91fd620a4b0a42786651330
Brendanej <56318922+Brendanej@users.noreply.github.com>
2020-11-22 22:41:33 +0000

    Update Module.java

--- src/main/java/Module.java
+++ src/main/java/Module.java
@@ -9,18 +9,19 @@
  * @author brend
  */
 public class Module {
+    //Initialising Strings and Array
     private String ModuleName;
     private String ID;
     private Student[] Students;
     private String CoursesAssociated;
-
+    //Constructor
     public Module(String ModuleName, String ID, Student[] Students,String CoursesAssociated) {
         this.ModuleName = ModuleName;
         this.ID = ID;
         this.Students = Students;
         this.CoursesAssociated=CoursesAssociated;
     }
-
+    //Getters
     public String getModuleName() {
         return ModuleName;
     }
@@ -37,8 +38,9 @@ public class Module {
     public Student[] getStudents() {
         return Students;
     }
+    
      public String getModuleInfo() {
-        String ModuleInfo = ModuleName +  ID +CoursesAssociated;
+        String ModuleInfo = ModuleName +  ID +CoursesAssociated;//Making and assigning Module info String.
         return ModuleInfo;
     }
 }
